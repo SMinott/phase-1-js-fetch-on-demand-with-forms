e2e100d23aa45bcbc9d56cfffcf42a602939ee9a
Susan Minott <susan_minott@yahoo.com>
2022-05-07 14:42:50 -0400

    commit tree 5cb6d0982604abf3f2ed5dc0d6741c291bbcc6b0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
-const init = () => {
-  
-}
+// const init = () => {
+//     const form = document.querySelector('form')
 
-document.addEventListener('DOMContentLoaded', init);
+//     form.addEventListener('submit', (event) => {
+//         event.preventDefault()
+//         // console.log(event)
+//         event.target.children[1].value
+//         console.log(children[1].value);
+//     })
+    
+// }
+
+// document.addEventListener('DOMContentLoaded', init);
+
+// function fetchMovie(){
+//     fetch('http://localhost:3000/movies')
+//     .then(res => res.json())
+//     .then(data => init(data))
+// } 
+
+
+const init = () => {
+    const inputForm = document.querySelector('form')
+  
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+  
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
